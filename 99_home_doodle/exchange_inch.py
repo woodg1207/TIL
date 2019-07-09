e4d1b7a64a6be2f4880ef1f08d441637e8179a12
woodg1207 <woodg1207@gmail.com>
2019-07-09 22:07:04 +0900

    doodle commit

---
+++ 99_home_doodle/exchange_inch.py
@@ -0,0 +1,21 @@
+##1.인치를 cm로 변환
+
+#inch = int(input('인치를 입력하시오'))
+#cm = inch * 2.54
+# print(f'{inch}inch는 변환하면 {cm}cm이다.')
+
+##2.kg을 파운드로 변환하는 프로그램 
+#소수점 자리수는 round(변수, 자리수)
+
+# kilo = float(input('킬로그램을 입력하세요'))
+# lb = kilo * 2.2046
+# print(f'{round(kilo,2)}kg => {round(lb,2)}lb')
+
+##3.섭씨를 화씨로 변경
+# do_c = float(input('입력해라'))
+# do_f = (do_c * 1.8) + 32
+# print(f'{do_c} => {round(do_f, 3)}')
+##4.화씨를 섭씨로 변경
+# do_f = float(input('insert'))
+# do_c = (do_f - 32) / 1.8
+# print(f'{do_f} => {round(do_c,2)}')
