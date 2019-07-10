edc8de7f5966063087cd2f460ac59f9bf9afe350
woodg1207 <woodg1207@gmail.com>
2019-07-10 17:38:36 +0900

    0710 commmit

--- 00_startcamp/03_day/flask/hello.py
+++ 00_startcamp/03_day/flask/hello.py
@@ -5,8 +5,10 @@ app = Flask(__name__)
 def hello():
     return 'This is ssafy world!!'
 
-@app.route('/ssafy')
-def ssafy():
-    return 'This is ssafy'
-
+@app.route('/ssafy') #마지막 주소의 /ssafy로 들어가면 어떤걸 할지 결정하도록함
+def ssafy():  ##함수를 정의하는 방법 
+    return 'This is ssafy!'
+@app.route('/ss')
+def bye():
+    return 'this'
 
