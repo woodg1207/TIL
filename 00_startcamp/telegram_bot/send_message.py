91da8a63fd5b4be2b5438c708b662d31d8a77919
woodg1207 <woodg1207@gmail.com>
2019-07-12 16:06:51 +0900

    0712 re commit

--- 00_startcamp/telegram_bot/send_message.py
+++ 00_startcamp/telegram_bot/send_message.py
@@ -5,6 +5,7 @@ from decouple import config
 api_url = 'https://api.telegram.org'
 token = config('TELEGRAM_BOT_TOKEN')
 chat_id = config('CHAT_ID')
+
 text = '안녕하세요'
 
 send_message = requests.get(f'{api_url}/bot{token}/sendMessage?chat_id={chat_id}&text={text}')
