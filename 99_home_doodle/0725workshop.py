d78c9e35d21d4d588eac931c2cca02cac18f5848
woodg1207 <woodg1207@gmail.com>
2019-07-25 17:57:24 +0900

    commit

--- 99_home_doodle/0725workshop.py
+++ 99_home_doodle/0725workshop.py
@@ -1,30 +1,44 @@
-class Animal:
+# class Animal:
 
-    def __init__(self, name):
-        self.name = name
+#     def __init__(self, name):
+#         self.name = name
     
-    def walk(self):
-        print(f'{self.name}! 걷는다!')
+#     def walk(self):
+#         print(f'{self.name}! 걷는다!')
     
-    def eat(self):
-        print(f'{self.name}! 먹는다!')
+#     def eat(self):
+#         print(f'{self.name}! 먹는다!')
 
-class Dog(Animal):
-    def walk(self):
-        print(f'{self.name}! 달린다!')
+# class Dog(Animal):
+#     def walk(self):
+#         print(f'{self.name}! 달린다!')
     
-    def run(self):
-        print(f'{self.name}! 달린다!')
+#     def run(self):
+#         print(f'{self.name}! 달린다!')
 
-class Bird(Animal):
-    def fly(self):
-        print(f'{self.name}! 푸드덕!')
+# class Bird(Animal):
+#     def fly(self):
+#         print(f'{self.name}! 푸드덕!')
 
-dog = Dog('멍멍이')
-dog.walk()
-dog.run()
+# dog = Dog('멍멍이')
+# dog.walk()
+# dog.run()
 
-bird = Bird('구구')
-bird.walk()
-bird.eat()
-bird.fly()
+# bird = Bird('구구')
+# bird.walk()
+# bird.eat()
+# bird.fly()
+
+def even_and_odd(numbers):
+    even = []
+    odd = []
+    for num in numbers:
+        if num%2:
+            odd.append(num)
+            odd = sorted(odd, reverse=True)
+    print(odd)
+    return odd
+print(even_and_odd([7, 3, 14, 17]))
+
+
+
