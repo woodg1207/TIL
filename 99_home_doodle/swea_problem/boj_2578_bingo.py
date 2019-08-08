b46d2c7b5d4b916cda165c9d976dae0c32267d03
woodg1207 <woodg1207@gmail.com>
2019-08-09 00:18:55 +0900

    commit

--- 99_home_doodle/swea_problem/boj_2578_bingo.py
+++ 99_home_doodle/swea_problem/boj_2578_bingo.py
@@ -4,15 +4,3 @@ a = []
 for i in range(5):
     a.extend(map(int, input().split()))
 
-bingo = 0
-while bingo < 3:
-    for idx, num in enumerate(a):
-        for i in range(len(my)):
-            for j in range(len(my[i])):
-                if my[i][j] == num:
-                    my[i][j] = 0
-                    cnt = idx
-                if sum(my)
-            if sum(my[i]) == 0:
-                bingo += 1
-                
