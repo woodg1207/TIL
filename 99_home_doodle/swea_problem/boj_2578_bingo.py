8db603d516dfb9284169d7d9461d989c4fa8f695
woodg1207 <woodg1207@gmail.com>
2019-08-26 17:40:00 +0900

    commit

--- 99_home_doodle/swea_problem/boj_2578_bingo.py
+++ 99_home_doodle/swea_problem/boj_2578_bingo.py
@@ -7,11 +7,11 @@ for i in range(5):
     t.extend(map(int, input().split()))
 row = []
 column = []
-diag1 = []
-diag2 = []
 while t:
     bingo = 0
     n = t.pop(0)
+    diag1 = []
+    diag2 = []
     for i in range(5):
         for j in range(5):
             if arr[i][j] == n:
@@ -20,12 +20,8 @@ while t:
             column.append(arr[j][i])
             if row.count(0) == 5:
                 bingo += 1
-                if bingo == 3:
-                    reusult = 25-len(t)
             if column.count(0) == 5:
                 bingo += 1
-                if bingo == 3:
-                    reusult = 25-len(t)
         else:
             row.clear()
             column.clear()
@@ -33,17 +29,9 @@ while t:
         diag2.append(arr[i][4-i])
         if diag1.count(0) == 5:
             bingo += 1
-            if bingo == 3:
-                    reusult = 25-len(t)
         if diag2.count(0) == 5:
-            bingo += 1
-            if bingo == 3:
-                    reusult = 25-len(t)       
-    else:
-        diag1.clear()
-        diag2.clear()
+            bingo += 1      
     if bingo >= 3:
-
+        reusult = 25-len(t)    
         break
-
 print(reusult)
