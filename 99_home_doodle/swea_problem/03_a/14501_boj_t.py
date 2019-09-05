744f8f55d7de7af9e901bc32df7e17546e97d412
woodg1207 <woodg1207@gmail.com>
2019-09-06 01:08:59 +0900

    c

--- 99_home_doodle/swea_problem/03_a/14501_boj_t.py
+++ 99_home_doodle/swea_problem/03_a/14501_boj_t.py
@@ -3,9 +3,12 @@ import sys; sys.stdin = open('t.txt', 'r')
 def dfs(s):
     work[s] = 1
     m = []
+    cnt = 0
     stack.append([s, info[s][0]])
     while stack:
         print(stack)
+        check = len(stack)
+        cnt += 1
         a = stack.pop()
         m.append(info[a[0]][1])
         print('money', m)
@@ -21,6 +24,7 @@ info = [list(map(int, input().split())) for _ in range(n)]
 info.insert(0, 0)
 stack = []
 ma = 0
+
 for i in range(1, n+1):
     if i+info[i][0] <= n+1:
         work = [0]*(n+1)
