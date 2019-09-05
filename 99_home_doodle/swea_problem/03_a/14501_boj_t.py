6b7e6b7e2857628425bf2d1cceb7dbc3a12a9243
woodg1207 <woodg1207@gmail.com>
2019-09-05 23:58:06 +0900

    commit boj

--- 99_home_doodle/swea_problem/03_a/14501_boj_t.py
+++ 99_home_doodle/swea_problem/03_a/14501_boj_t.py
@@ -1,15 +1,28 @@
 import sys; sys.stdin = open('t.txt', 'r')
 
+def dfs(s):
+    work[s] = 1
+    m = []
+    stack.append([s, info[s][0]])
+    while stack:
+        print(stack)
+        a = stack.pop()
+        m.append(info[a[0]][1])
+        print('money', m)
+        for i in range(a[0]+a[1],n+1):
+            if i+info[i][0] <= n+1:
+                work[i] = 1
+                stack.append([i, info[i][0]])
+
+    
+
 n = int(input())
 info = [list(map(int, input().split())) for _ in range(n)]
 info.insert(0, 0)
 stack = []
+ma = 0
 for i in range(1, n+1):
-    if i+info[i][0] <= n:
-        stack.append([i, info[i][0]])
-while stack:
-    d = stack.pop() # 첫 일정
-    m = []
-    m.append(info[d[0]][1])
-    for i in range(d[0]+d[1],n+1): # 다음일정
-        if info[i][1]
+    if i+info[i][0] <= n+1:
+        work = [0]*(n+1)
+        dfs(i)
+        
