d5f1ccf83e21e793a3a81ea2a134903bbb281659
woo <woodg1207@gmail.com>
2019-09-17 20:00:30 +0900

    commit dfs

--- 99_home_doodle/swea_problem/04_bochoong_0917/4875_swea_miro.py
+++ 99_home_doodle/swea_problem/04_bochoong_0917/4875_swea_miro.py
@@ -1,8 +1,52 @@
 import sys
 sys.stdin = open('m.txt', 'r')
 
+# def dfs(a, b):
+#     global re
+#     visit.append([a, b])
+#     print(visit)
+#     if arr[a][b] == 3:
+#         re = 1
+#         return
+#     if a+1<n  and arr[a+1][b]!=1 and [a+1, b] not in visit:
+#         dfs(a+1,b)
+#     if b+1<n and arr[a][b+1]!=1 and [a, b+1] not in visit:
+#         dfs(a, b+1)
+#     if 0<=a-1 and arr[a-1][b]!=1 and [a-1, b] not in visit:
+#         dfs(a-1, b)
+#     if 0<=b-1 and arr[a][b-1]!=1 and [a, b-1] not in visit:
+#         dfs(a, b-1)
+
+def dfs(a, b):
+    global re
+    print(a, b)
+    if arr[a][b] == 3:
+        print('dddd')
+        re = 1
+        return
+    if a+1<n and arr[a+1][b]==0:
+        arr[a + 1][b]=1
+        dfs(a+1,b)
+    if b+1<n and arr[a][b+1]==0:
+        arr[a][b+1] = 1
+        dfs(a, b+1)
+    if 0<=a-1 and arr[a-1][b]==0:
+        arr[a-1][b]=1
+        dfs(a-1, b)
+    if 0<=b-1 and arr[a][b-1]==0:
+        arr[a][b-1]=1
+        dfs(a, b-1)
+
 tc = int(input())
 for sample in range(tc):
     n = int(input())
     arr = [[int(i) for i in input()] for _ in range(n)]
-    
+    visit = []
+    re = 0
+    for i in range(n):
+        for j in range(n):
+            if arr[i][j] == 2:
+                arr[i][j] = 1
+                dfs(i, j)
+                break
+    print('#{} {}'.format(sample+1, re))
