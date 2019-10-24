4a2e7521afb3fe866d4de03e086b95d9b58d04eb
woo <woodg1207@gmail.com>
2019-10-24 19:48:43 +0900

    commit django

--- 99_home_doodle/test_1024/accounts/views.py
+++ 99_home_doodle/test_1024/accounts/views.py
@@ -1,19 +1,19 @@
 from django.shortcuts import render, redirect
-from django.contrib.auth.forms import AuthenticationForm
+from django.contrib.auth.forms import AuthenticationForm, UserCreationForm
 from django.contrib.auth import login as auth_login
 from .forms import CustomUserCreationForm, CustomUserChangeForm
 # Create your views here.
 def signup(request):
-    if request.user.is_authenticated:
-        return redirect('articles:index')
+    # if request.user.is_authenticated:
+    #     return redirect('articles:index')
     if request.method == 'POST':
-        form = CustomUserCreationForm(request.POST)
+        form = UserCreationForm(request.POST)
         if form.is_valid():
             user = form.save()
             auth_login(request, user)
             return redirect('articles:index')
     else:
-        form = CustomUserCreationForm()
+        form = UserCreationForm()
     context = {'form':form, }
     return render(request, 'accounts/signup.html', context)
 
