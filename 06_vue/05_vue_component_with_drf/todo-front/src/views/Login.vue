2295d8ea8687fceb0ed213a9d39238ae6406a442
woo <woodg1207@gmail.com>
2019-11-18 15:53:15 +0900

    commit vue

---
+++ 06_vue/05_vue_component_with_drf/todo-front/src/views/Login.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <LoginForm/>
+  </div>
+</template>
+
+<script>
+  // @ 는 /src의 alias
+  // import LoginForm from '../components/LoginForm' // 상대경로.
+  import LoginForm from '@/components/LoginForm'  //절대 경로
+  export default {
+    name:'login',
+    components:{
+      LoginForm,
+    }
+  }
+</script>
+
+<style >
+
+</style>
