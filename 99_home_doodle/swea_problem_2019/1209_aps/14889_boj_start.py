a204162ffafa4bdb50cf34883edd46a729a89fa4
Hyun swdev <hyunssafy@gmail.com>
2019-12-11 17:54:48 +0900

    commit

--- 99_home_doodle/swea_problem_2019/1209_aps/14889_boj_start.py
+++ 99_home_doodle/swea_problem_2019/1209_aps/14889_boj_start.py
@@ -5,4 +5,34 @@ N = int(input())
 arr = [list(map(int, input().split())) for _ in range(N)]
 player = [i for i in range(N)]
 
+def build(cnt):
+    if cnt == N/2:
+        A, B = [], []
+        for i in range(N):
+            if v[i]:
+                A.append(i)
+            else:
+                B.append(i)
+        synergy(A)
+        return
+    for i in range(N):
+        if v[i]:continue
+        l.append(i)
+        v[i] = True
+        build(cnt+1)
+        l.pop()
+        v[i] = False
 
+def synergy(a):
+    for i in range(len(a)):
+        s = []
+        s.append(a[i])
+        for j in range(len(a)):
+            
+
+for i in range(N//2):
+    l = []
+    v = [False] * N
+    l.append(i)
+    v[i] = True
+    build(1)
