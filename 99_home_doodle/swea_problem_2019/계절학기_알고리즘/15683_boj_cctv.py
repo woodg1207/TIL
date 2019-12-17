a1c30243fef387341fffd72532873993aed78104
woo <woodg1207@gmail.com>
2019-12-18 00:06:48 +0900

    commit cctv

--- 99_home_doodle/swea_problem_2019/계절학기_알고리즘/15683_boj_cctv.py
+++ 99_home_doodle/swea_problem_2019/계절학기_알고리즘/15683_boj_cctv.py
@@ -1,6 +1,6 @@
 import sys; sys.stdin = open('cc.txt', 'r')
 
-def cctvdir():
+def cctv_dir(cctvd):
     
     return
 
@@ -8,23 +8,18 @@ N, M = map(int, input().split())
 arr = [list(map(int, input().split())) for _ in range(N)]
 visit = [[False]*M for _ in range(N)]
 cctv = []
-cctv_dir = []
+
 office = N*M
 for i in range(N):
     for j in range(M):
         if 0<arr[i][j]<=5:
-            cctv.append((i, j, arr[i][j]))
             if arr[i][j] == 2:
-                cctv_dir.append(2)
+                d = 2
             elif arr[i][j] == 5:
-                cctv_dir.append(1)
+                d = 1
             else: #1, 3, 4
-                cctv_dir.append(4)
+                d = 4
+            cctv.append((i, j, d))
         elif arr[i][j] == 6:
             office -= 1
 office -= len(cctv)
-for i in range(len(cctv)):
-    d = cctv_dir[i]
-    while d:
-        d-=1
-        
