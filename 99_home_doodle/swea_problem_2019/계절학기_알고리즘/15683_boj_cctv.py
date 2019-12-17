0185dab7d2b5d55704cecdfa9f9bf4371be3a365
woodg1207 <woodg1207@gmail.com>
2019-12-17 13:49:11 +0900

    pipe dp

--- 99_home_doodle/swea_problem_2019/계절학기_알고리즘/15683_boj_cctv.py
+++ 99_home_doodle/swea_problem_2019/계절학기_알고리즘/15683_boj_cctv.py
@@ -2,3 +2,13 @@ import sys; sys.stdin = open('cc.txt', 'r')
 
 N, M = map(int, input().split())
 arr = [list(map(int, input().split())) for _ in range(N)]
+visit = [[False]*M for _ in range(N)]
+cctv = []
+office = N*M
+for i in range(N):
+    for j in range(M):
+        if 0<arr[i][j]<=5:
+            cctv.append((i, j, arr[i][j]))
+        elif arr[i][j] == 6:
+            office -= 1
+office -= len(cctv)
--- 99_home_doodle/swea_problem_2019/계절학기_알고리즘/17070_boj_2_pipe.py
+++ 99_home_doodle/swea_problem_2019/계절학기_알고리즘/17070_boj_2_pipe.py
@@ -1,13 +1,19 @@
 import sys; sys.stdin = open('p.txt', 'r')
 
+import sys
+sys.setrecursionlimit(10**6)
 def find(r, c, dir):
     global cnt
     if r == N-1 and c == N-1:
         cnt += 1
-        return
+        w = 1
+        return w
+    if memo[dir][r][c] != -1:
+        return memo[dir][r][c]
     l = 3
     if dir != 1:
         l = 2
+    w = 0
     for i in range(l):
         if dir == 2:
             i += 1
@@ -29,8 +35,9 @@ def find(r, c, dir):
             elif dir == 2:
                 if i == 2:
                     if nr == N - 1 and nc != N - 1: continue
-            find(nr, nc, i)
-
+            w += find(nr, nc, i)
+    memo[dir][r][c] = w
+    return w
 
 import time
 start = time.time()  # 시작 시간 저장
@@ -40,8 +47,7 @@ dc = (1, 1, 0)
 N = int(input())
 arr = [list(map(int, input().split())) for _ in range(N)]
 cnt = 0
-memo = [[[False]*N for _ in range(N)] for _ in range(3)]
-find(0, 1, 0)
-print(cnt)
-print("time :", time.time() - start)
+memo = [[[-1]*N for _ in range(N)] for _ in range(3)]
+print(find(0, 1, 0))
 
+print("time :", time.time() - start)
