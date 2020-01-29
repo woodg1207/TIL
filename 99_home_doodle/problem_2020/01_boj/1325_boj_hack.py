6c95a6ea7b95534e871571796a724e14ed99990c
woo <woodg1207@gmail.com>
2020-01-30 01:25:56 +0900

    commit boj_1325

--- 99_home_doodle/problem_2020/01_boj/1325_boj_hack.py
+++ 99_home_doodle/problem_2020/01_boj/1325_boj_hack.py
@@ -6,17 +6,42 @@ def dfs(n):
         if G[n][i] == 0: continue
         if visit[G[n][i]]: continue
         dfs(G[n][i])
+    return
+
+from collections import deque
+def bfs(x):
+    q = deque()
+    q.append(x)
+    visit[x] = True
+    while q:
+        nx = q.popleft()
+        print(G[nx])
+        # if visit[G[nx][i]]: continue
+        for i in range(len(G[nx])):
+            if visit[G[nx][i]]: continue
+            visit[G[nx][i]] = True
+            q.append(G[nx][i])
 
 N, M = map(int, input().split())
-G = [[]]*(N+1)
+G = [[False]]*(N+1)
+# print(G)
 for i in range(M):
     a, b = map(int, input().split())
-    G[b].append(a)
-    
+    # print(G[a][0])
+    if G[b][0]:
+        G[b].append(a)
+    else:
+        G[b] = [a]
+    # print(a, b)
+    # print(G)
+
 visit = [False]*(N+1)    
 result = []
 for i in range(1, N+1):
     if visit[i]: continue
-    dfs(i)
+    
+    # dfs(i)
+    bfs(i)
+    print(visit)
     result.append(i)
 print(*result)
