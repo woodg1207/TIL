3b34a03e3e4be0d7b9c5858e06b78d0b07166a65
woo <woodg1207@gmail.com>
2020-02-03 01:51:46 +0900

    commit

--- 99_home_doodle/problem_2020/02_boj/14501_boj_retire.py
+++ 99_home_doodle/problem_2020/02_boj/14501_boj_retire.py
@@ -1,2 +1,22 @@
 import sys; sys.stdin = open('r.txt', 'r')
 
+N = int(input())
+info = [list(map(int, input().split())) for _ in range(N)]
+M = 0
+
+def work(day):
+    global money, M
+    if day >N:return
+    if money > M:
+        M = money
+    for i in range(day, N):
+        if  info[i][0] > N: continue
+        money += info[i][1]
+        work(i+ info[i][0])
+        money -= info[i][1]
+
+for i in range(N):
+    if i + info[i][0] > N: continue
+    money = info[i][1]
+    work(i + info[i][0])
+print(M)
