ef70bfea5d0083b5d7eed8b046c245d205843e34
woo <woodg1207@gmail.com>
2020-02-07 01:17:52 +0900

    commit aps 17135

--- 99_home_doodle/problem_2020/02_boj/17135_boj_castle.py
+++ 99_home_doodle/problem_2020/02_boj/17135_boj_castle.py
@@ -8,7 +8,8 @@ def attack(arc, line):
     attacked = [[False]*(M) for _ in range(N)]
     # for i in arc:
     #     for mop in mops:
-            
+    # D 안에들어오는 mop은 리스트로 추가를 하고 (i, j, D) 
+    # 거리가 작은 애들중에서 j값이 작은 애로 공격을 한다..
     return
 
 
