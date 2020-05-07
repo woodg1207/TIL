c8f442d3070d8b8964934b0bf8fe88a0d8216864
woo <woodg1207@gmail.com>
2020-05-08 00:05:26 +0900

    test kakao

---
+++ 02_/kakao_dev/rec.py
@@ -0,0 +1,34 @@
+
+def solution(v):
+    
+    check = []
+    visit = []
+    for point in v:
+        flag = 0
+        for i in range(len(point)):
+            if [point[i], i] in check: 
+                visit.append(point)
+                flag = 1
+                break
+        if flag: continue
+        for i in range(2):
+            check.append([point[i], i])
+    v = [False] * 4
+    for idx, i in enumerate(visit[0]):
+        for j in range(len(check)):
+            if i == check[j][0] and check[j][1]==idx:
+                v[j]=True
+    result = [0,0]
+    for i in range(4):
+        if v[i]: continue
+        if check[i][1]:
+            result[1]=check[i][0]
+        else:
+            result[0]=check[i][0]
+            
+
+    return result
+
+
+sample =[[1, 1], [2, 2], [1, 2]]
+print(solution(sample))
