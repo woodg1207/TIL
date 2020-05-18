d8d6fd636e72b988846f463df8b65708637a57df
woodg1207 <woodg1207@gmail.com>
2020-05-18 13:06:37 +0900

    rasp

--- 99_home_doodle/problem_2020/programmers/전자.py
+++ 99_home_doodle/problem_2020/programmers/전자.py
@@ -5,5 +5,5 @@ while b<=10:
     cnt += 1
     b += i
     i += 1
-    print('{}번째 while : b={}, i={}'.format(cnt, b,i))
+    print('{}번째 while : b = {}, i = {}'.format(cnt, b,i))
 print('i={}, b={}'.format(i,b))
