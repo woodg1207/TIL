6380af61cc9673332105c833e492fabdb708d0da
woodg1207 <woodg1207@gmail.com>
2020-06-09 03:23:53 +0900

    time delta

--- 02_/finalproject/test/time.py
+++ 02_/finalproject/test/time.py
@@ -1,6 +1,12 @@
-import time
+# import time
 
-t = time.localtime()
-# print(t)
-t1 = time.strftime('%Y-%m-%d %X', t)
-print(t1)
+# t = time.localtime()
+# # print(t)
+# t1 = time.strftime('%Y-%m-%d %X', t)
+# print(t1)
+
+
+from datetime import timedelta
+t = timedelta()
+print(type(t))
+print(t)
