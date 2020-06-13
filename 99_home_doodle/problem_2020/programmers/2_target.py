5e5223b29d2c40f4f4217e27c530332235c784ad
woodg1207 <woodg1207@gmail.com>
2020-06-13 21:04:28 +0900

    solve aps

--- 99_home_doodle/problem_2020/programmers/2_target.py
+++ 99_home_doodle/problem_2020/programmers/2_target.py
@@ -1,23 +1,25 @@
 
 def solution(numbers, target):
-    l=[]
+    n = len(numbers)
     cnt = 0
-    for i in range(len(l)):
-        result = 0
-        for j in range(len(l[i])):
-            if l[i][j]:
-                result += numbers[j]
+    for i in range(2**n):
+        number = 0
+        num = bin(i)[2:]
+        extra = n-len(num)
+        result = '0'*extra + num
+        for j in range(len(result)):
+            if result[j]=='1':
+                number += numbers[j]
             else:
-                result -= numbers[j]
-            if not (result<1000):
-                break
-        if result == target:
+                number -= numbers[j]
+        if number == target:
             cnt += 1
+
+
     return cnt
 
 
 
 N = [1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1]
 T = 3
-# print(solution(N, T))
-print(bin(2**4-1))
+print(solution(N, T))
--- 99_home_doodle/r.py
+++
@@ -1,8 +0,0 @@
-import random
-
-
-random_str = ''
-il = ['I', 'l']
-for i in range(20):
-    random_str += random.choice(il)
-print(random_str)
