d5d79cf219a5d63ad79ad734ef90b2c3b11af5ae
woodg1207 <woodg1207@gmail.com>
2020-06-23 19:25:45 +0900

    add 자소설 컨설팅

--- 99_home_doodle/problem_2020/programmers/stack_queue1.py
+++ 99_home_doodle/problem_2020/programmers/stack_queue1.py
@@ -1,5 +1,13 @@
+from collections import deque
+
+
 def solution(bridge_length, weight, truck_weights):
     answer = 0
+    q = deque()
+    q.extend(truck_weights)
+    b = []
+    while q:
+        
     return answer
 
 
