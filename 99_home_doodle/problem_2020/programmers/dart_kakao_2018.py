d2acfdc9f66747622c64f25d79dfeda3567673eb
woodg1207 <woodg1207@gmail.com>
2020-07-03 00:38:39 +0900

    programmers aps

---
+++ 99_home_doodle/problem_2020/programmers/dart_kakao_2018.py
@@ -0,0 +1,36 @@
+
+point_dict = {
+    'S':1,
+    'D':2,
+    'T':3,
+}
+num = '1234567890'
+option = '*#'
+def solution(dartResult):
+    answer = 0
+    point = ''
+    point_list = []
+    for idx, n in enumerate(dartResult):
+        if n in num: # 숫자 판별
+            point += n
+        else:
+            if point: 
+                point_list.append(int(point))
+                point = ''            
+            if n not in option:
+                point_list[-1] = point_list[-1]**point_dict[n]
+            else:
+                l = len(point_list)
+                if n == '*':
+                    if l == 1:
+                        point_list[l-1] = point_list[l-1] * 2
+                    else:
+                        point_list[l-2] = point_list[l-2] * 2
+                        point_list[l-1] = point_list[l-1] * 2
+                else:
+                    point_list[l-1] = point_list[l-1] * -1
+    return sum(point_list)
+
+
+testcase = '1D2S#10S'
+print(solution(testcase))
