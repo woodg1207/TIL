d7fefd4627257a372c71af611d15edc16445a808
woodg1207 <woodg1207@gmail.com>
2020-08-01 17:24:11 +0900

    toyproject vue

---
+++ 03_toyprojects/hobby/todo-big5/src/App.vue
@@ -0,0 +1,24 @@
+<template>
+  <v-app>
+    <big-test :questions="questions"></big-test>
+  </v-app>
+</template>
+
+<script>
+  // import HelloWorld from './components/HelloWorld';
+  import BigTest from './components/personality/Big5Test';
+
+  export default {
+    name: 'App',
+
+    components: {
+      BigTest,
+    },
+
+    data() {
+      return {
+        questions: [],
+      }
+    }
+  };
+</script>
---
+++ 03_toyprojects/hobby/todo-big5/src/components/personality/Big5Test.vue
@@ -0,0 +1,31 @@
+<template>
+  <h1>hello</h1>
+  <ul v-for="question in questions">
+    <p>{{question.content}}</p>
+  </ul>
+</template>
+
+<script>
+  export default {
+    name: 'Big5Test',
+    data: {
+      questions: [{
+          content: '1'
+        },
+        {
+          content: '2'
+        },
+      ]
+    },
+    props: {
+      questions: {
+        type: Array,
+
+      }
+    }
+  }
+</script>
+
+<style>
+
+</style>
