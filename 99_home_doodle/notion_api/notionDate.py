a35aa2707bced6885fdb0a58c97b7ff52da6f7e2
woodg1207 <woodg1207@gmail.com>
2021-02-08 17:54:22 +0900

    fix notion

--- 99_home_doodle/notion_api/notionDate.py
+++ 99_home_doodle/notion_api/notionDate.py
@@ -12,23 +12,6 @@ notion_url = config('notion_url')
 client = NotionClient(token_v2=token)
 page = client.get_block(notion_url)
 
-
-def notion(title, info):
-    print(title)
-    day_page = page.children.add_new(PageBlock)
-    day_page.title = title
-    for i in ['아침','점심','저녁']:
-        todo = day_page.children.add_new(TodoBlock)
-        todo.title = i
-        todo.checked = False
-
-    for key, value in info.items():
-        if value == 1:
-            todo = day_page.children.add_new(TodoBlock)
-            todo.title = key
-            todo.checked = False
-    
-
 info = dict()
 start = date(year=2021, month=2, day=4)
 d = timedelta(days=1)
@@ -36,8 +19,8 @@ cnt = 1
 title_list = []
 today = date.today()
 print(today)
-for i in range(5):
-    if today>start:
+for i in range(40):
+    if today>=start:
         start += d
         cnt *= -1
         continue
@@ -48,15 +31,36 @@ for i in range(5):
     if not i%3:
         x = 1
     info[title]={
-        'shampooA':cnt,
-        'shampooB':-cnt,
-        'threedays':x,
-        'pill':-cnt
+        'no':cnt, #샴푸 : 노비프록스
+        'jin':-cnt, # 샴푸 : 진크피
+        0:x, ## 저녁. '3일 저녁 : 제로큐탄'
+        1:-cnt # '2일 : 아보다트'
     }
     start += d
     cnt *= -1
-pprint(info)
-print(title_list)
+# pprint(info)
+# print(title_list)
 for title in title_list:
-    break
-    notion(title, info[title])
+    print(title)
+    day_page = page.children.add_new(PageBlock)
+    day_page.title = title
+    l = ['아침','점심','저녁']
+    for i in range(3):
+        text = l[i]
+        if i==2:
+            for j in range(2):
+                if info[title][j]==1:
+                    if j:
+                        text += ' + 아보다트'
+                    else:
+                        text += ' + 제로큐탄'
+                else:
+                    continue
+        elif i == 0:
+            if info[title]['no'] == 1:
+                text += ' + 샴푸:노비프록스'
+            else:
+                text += ' + 샴푸:진크피'
+        todo = day_page.children.add_new(TodoBlock)
+        todo.title = text
+        todo.checked = False
